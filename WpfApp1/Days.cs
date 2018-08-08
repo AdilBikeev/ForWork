8ef421ae3ec9447eb8eafcf40c2771292fe5a7c9
Адиль <bikeev1999@mail.ru>
2018-08-08 14:23:19 +0300

    Создал класс people чтобы запихнуть их в checkbox

---
+++ WpfApp1/Days.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Peoples
+    {
+        public string FIO { get; set; }
+        public Peoples(string FIO)
+        {
+            this.FIO = FIO;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Peoples> peoples = new List<Peoples>();//список людей, которые работали в данный день
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            peoples.Add(new Peoples("Я"));
+            peoples.Add(new Peoples("Ангелина Кузнецов"));
+            peoples.Add(new Peoples("Фатима Османова"));
+            peoples.Add(new Peoples("Света Ягъяева"));
+            peoples.Add(new Peoples("Молодой парень"));
+            peoples.Add(new Peoples("Марина Семёнова"));
+        }
+    }
+}
